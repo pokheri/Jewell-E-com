38437ccfccfcfe5315761586844f1cf5e1beb510
pokheri <pokheridinesh333@gmail.com>
2026-01-25 10:18:44 +0530

    Jweller setup tailwind/breeze/laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard Page</title>
+    @Vite('resources/css/app.css')
+</head>
+
+<body>
+
+    <div class="bg-red-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequuntur numquam vel
+        doloribus adipisci, mollitia quidem veritatis eius, praesentium doloremque quam hic. Voluptatum, quaerat
+        repellendus vero a delectus recusandae est.</div>
+
+</body>
+
+</html>
